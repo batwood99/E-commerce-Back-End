64a7bdf71510fea7a2e4de6868d316dc61cfab80
batwood99 <atwood@mail.usf.edu>
2023-07-15 12:08:12 -0400

    added product routes code

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,35 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include: [Category, Tag],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json({ error: 'An error occurred while retrieving the products.' });
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [Category, Tag],
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'This product does not exist.' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json({ error: 'An error occurred while retrieving the product.' });
+  }
 });
 
 // create new product
@@ -92,8 +112,14 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const deletedProduct = await Product.destroy({ where: { id: req.params.id } });
+    res.status(200).json({ message: 'Product deleted successfully.' });
+  } catch (err) {
+    res.status(500).json({ error: 'An error occurred while deleting the product.' });
+  }
 });
 
 module.exports = router;
